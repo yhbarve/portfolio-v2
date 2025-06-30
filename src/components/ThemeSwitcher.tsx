f0d621e6ca1ce379eb6f83ae02b81ad363efff79
Yash Barve <90118570+yhbarve@users.noreply.github.com>
2025-06-30 01:30:00 -0400

    revert to old themes

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,7 +1,7 @@
 // app/components/ThemeSwitcher.tsx
 "use client";
 
-import { useTheme } from "next-themes";
+import {useTheme} from "next-themes";
 import { useEffect, useState } from "react";
 
 export function ThemeSwitcher() {
@@ -14,15 +14,17 @@ export function ThemeSwitcher() {
 
   if(!mounted) return null
 
-  return (
-    <select
-      className="py-1 px-2 text-sm rounded-md border bg-background text-foreground"
-      value={theme}
-      onChange={(e) => setTheme(e.target.value)}
-    >
-      <option value="light">Light Mode</option>
-      <option value="dark">Dark Mode</option>
-      <option value="baby-blue">Baby Blue</option>
-    </select>
-  )
+  if (theme == 'light'){
+    return (
+      <div className="flex items-center">
+        <button className="lg:text-zinc-950 lg:bg-none py-1 px-2 border lg:border-transparent rounded-md text-sm hover:border hover:bg-zinc-50 transition ease-in-out hover:border-zinc-300 hover:backdrop-blur-2xl font-medium bg-zinc-50 text-zinc-950 border-zinc-300" onClick={() => setTheme('dark')}>Dark Mode</button>
+      </div>
+    )
+  } else {
+    return (
+      <div className="flex gap-3">
+        <button className="text-zinc-50 transition ease-in-out py-1 px-2 rounded-md text-sm dark:hover:bg-zinc-900 dark:hover:text-white hover:text-zinc-950 border lg:border-transparent hover:border dark:hover:border-zinc-800 hover:backdrop-blur-2xl font-medium border-zinc-800 bg-zinc-900 lg:bg-none" onClick={() => setTheme('light')}>Light Mode</button>
+      </div>
+    )
+  }
 };
