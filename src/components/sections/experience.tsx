cbc72c2ebdf3b559123ef268cfa882948906df6b
yhbarve <yhbarve@uwaterloo.ca>
2025-07-28 20:13:40 -0400

    updated fall 2025 co-op

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -1,19 +1,78 @@
 import ExperienceCard from "../ExperienceCard";
 
-export default function Experience(){
-    return (
-        <div id="experiences" className="pt-24">
-            <div className="text-sm font-bold pb-2 pl-2 cursor-default text-section-header">EXPERIENCE</div>
-            <div className="flex flex-col gap-4 text-card-foreground hover:text-card-otherHoverForeground">
-                <ExperienceCard year="SEP — DEC 2024" title="Search Engine Optimization Co-op Student" org="AutoTrader - Trader Corporation" desc="Optimized website performance metrics, including Core Web Vitals, to enhance user experience and search engine rankings. Utilized Screaming Frog SEO Spider to collect and analyze data, identifying actionable insights and trends to inform optimization strategies. Conducted in-depth research on various social media platforms, contributing to the development and refinement of the organization's social media marketing strategy. Acquired and applied advanced SEO concepts, ensuring alignment with industry best practices and keeping pace with the latest updates in browser technologies and SEO methodologies." skills={["performance optimization", "data analysis", "python", "screaming frog seo", "chrome developer tools"]} />
-                
-                <ExperienceCard year="JAN — APR 2024" title="Software Developer Intern" org="Petrovision International LLC" desc="Contributed towards the redevelopment of the company's website. Developed frontend components using React and TailwindCSS. Regularly interacted with a team of senior developers. Performed some ad-hoc data analysis tasks. Helped in the development of training modules for the staff." skills={["react.js", "javascript", "tailwindcss", "component building", "git"]} />
+export default function Experience() {
+  return (
+    <div id="experiences" className="pt-24">
+      <div className="text-sm font-bold pb-2 pl-2 cursor-default text-section-header">
+        EXPERIENCE
+      </div>
+      <div className="flex flex-col gap-4 text-card-foreground hover:text-card-otherHoverForeground">
+        <ExperienceCard
+          year="SEP — DEC 2025"
+          title="Incoming Software Developer Intern"
+          org="Food For Good"
+          desc="Will be updated upon completion of the co-op term."
+          skills={[]}
+        />
 
-                <ExperienceCard year="MAR — MAY 2023" title="Data Analyst Co-op Student" org="Lazaridis School of Business and Economics" desc="Performed data gathering, cleaning and analysis for the marketing team using Microsoft Excel and Python. Used Tableau to create insightful data visualizations. Identified key trends and optimized the marketing strategy to reduce the spending by 15%." skills={["microsoft excel", "python", "tableau", "data analysis"]} />
+        <ExperienceCard
+          year="SEP — DEC 2024"
+          title="Search Engine Optimization Co-op Student"
+          org="AutoTrader - Trader Corporation"
+          desc="Optimized website performance metrics, including Core Web Vitals, to enhance user experience and search engine rankings. Utilized Screaming Frog SEO Spider to collect and analyze data, identifying actionable insights and trends to inform optimization strategies. Conducted in-depth research on various social media platforms, contributing to the development and refinement of the organization's social media marketing strategy. Acquired and applied advanced SEO concepts, ensuring alignment with industry best practices and keeping pace with the latest updates in browser technologies and SEO methodologies."
+          skills={[
+            "performance optimization",
+            "data analysis",
+            "python",
+            "screaming frog seo",
+            "chrome developer tools",
+          ]}
+        />
 
-                <ExperienceCard year="OCT 2022 — AUG 2023" title="Executive Member - Graphic Design Team" org="University of Waterloo's Data Science Club" desc="Interacted with event organizers to understand the requirements of the design. Used Canva to create posts for Instagram, LinkedIn, Facebook, and 𝕏. Briefly contributed to the club website design using Figma." skills={["canva", "figma", "graphic design", "teamwork", "leadership"]} />
-            </div>
-            <div className="mt-4 transition duration-200 ease-in-out text-linkToPage-foreground hover:translate-x-1"><a href="https://drive.google.com/file/d/1NGWDMo516Z43ypY47AfZoCXy88gmJBZy/view?usp=sharing" className="">View Full Resume →</a></div>
-        </div>
-    )
+        <ExperienceCard
+          year="JAN — APR 2024"
+          title="Software Developer Intern"
+          org="Petrovision International LLC"
+          desc="Contributed towards the redevelopment of the company's website. Developed frontend components using React and TailwindCSS. Regularly interacted with a team of senior developers. Performed some ad-hoc data analysis tasks. Helped in the development of training modules for the staff."
+          skills={[
+            "react.js",
+            "javascript",
+            "tailwindcss",
+            "component building",
+            "git",
+          ]}
+        />
+
+        <ExperienceCard
+          year="MAR — MAY 2023"
+          title="Data Analyst Co-op Student"
+          org="Lazaridis School of Business and Economics"
+          desc="Performed data gathering, cleaning and analysis for the marketing team using Microsoft Excel and Python. Used Tableau to create insightful data visualizations. Identified key trends and optimized the marketing strategy to reduce the spending by 15%."
+          skills={["microsoft excel", "python", "tableau", "data analysis"]}
+        />
+
+        <ExperienceCard
+          year="OCT 2022 — AUG 2023"
+          title="Executive Member - Graphic Design Team"
+          org="University of Waterloo's Data Science Club"
+          desc="Interacted with event organizers to understand the requirements of the design. Used Canva to create posts for Instagram, LinkedIn, Facebook, and 𝕏. Briefly contributed to the club website design using Figma."
+          skills={[
+            "canva",
+            "figma",
+            "graphic design",
+            "teamwork",
+            "leadership",
+          ]}
+        />
+      </div>
+      <div className="mt-4 transition duration-200 ease-in-out text-linkToPage-foreground hover:translate-x-1">
+        <a
+          href="https://drive.google.com/file/d/1NGWDMo516Z43ypY47AfZoCXy88gmJBZy/view?usp=sharing"
+          className=""
+        >
+          View Full Resume →
+        </a>
+      </div>
+    </div>
+  );
 }
