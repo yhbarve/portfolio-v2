050913c880694c55e5ca5f16db796ddd0af84407
yhbarve <yhbarve@uwaterloo.ca>
2025-08-18 17:30:31 -0500

    updated experience

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -40,7 +40,7 @@ export default function Experience() {
 
         <ExperienceCard
           year="JAN — APR '24"
-          title="Software Developer Intern"
+          title="Software Developer"
           org="Petrovision International LLC"
           desc="Contributed towards the redevelopment of the company's website. Developed frontend components using React and TailwindCSS. Regularly interacted with a team of senior developers. Performed some ad-hoc data analysis tasks. Helped in the development of training modules for the staff."
           skills={[
@@ -56,7 +56,7 @@ export default function Experience() {
 
         <ExperienceCard
           year="MAR — MAY '23"
-          title="Data Analyst Co-op Student"
+          title="Data Engineer"
           org="Lazaridis School of Business and Economics"
           desc="Performed data gathering, cleaning and analysis for the marketing team using Microsoft Excel and Python. Used Tableau to create insightful data visualizations. Identified key trends and optimized the marketing strategy to reduce the spending by 15%."
           skills={["microsoft excel", "python", "tableau", "data analysis"]}
@@ -64,7 +64,7 @@ export default function Experience() {
 
         <ExperienceCard
           year="OCT '22 — AUG '23"
-          title="Executive Member - Graphic Design Team"
+          title="Web Designer"
           org="University of Waterloo's Data Science Club"
           desc="Interacted with event organizers to understand the requirements of the design. Used Canva to create posts for Instagram, LinkedIn, Facebook, and 𝕏. Briefly contributed to the club website design using Figma."
           skills={[
