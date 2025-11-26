34a24b15927d170260eebb34c49c65a6593a3e4a
yhbarve <yhbarve@uwaterloo.ca>
2025-11-25 22:29:41 -0500

    Fix alignment b/w left side & right side

--- src/components/sections/left-side.tsx
+++ src/components/sections/left-side.tsx
@@ -7,7 +7,7 @@ import Navigation from "../Navigation";
 export default function LeftSide() {
   return (
     <div className="">
-      <div className="lg:sticky lg:top-0 lg:flex lg:h-screen lg:flex-col lg:justify-between lg:py-[6vh]">
+      <div className="lg:sticky lg:top-0 lg:flex lg:h-screen lg:flex-col lg:justify-between lg:py-24">
         <div className="sticky top-0 flex flex-col lg:h-full justify-between">
           <AboutMe />
           <div className="lg:block hidden lg:mt-[2.5vh]">
